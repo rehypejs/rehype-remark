e20d456f5ecdaa6d8952d9aa57f90bc682abd8b5
Chris Bush <chris.bush@mongodb.com>
2021-06-28 23:49:10 -0400

    Go for 100% type coverage
    - NOTE: Update to bridge transformer. Type complaints about done not calling next with complete parameters
    led me to unravel it and remove done/next altogether. Tests are passing, but let me know if this subtly
    breaks something.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 'use strict'
+/**
+ * @typedef {import('unified').FrozenProcessor} FrozenProcessor
+ * @typedef {import('unified').RunCallback} RunCallback
+ * @typedef {import('unified').Settings} Settings
+ * @typedef {import('unified').Transformer} Transformer
+ * @typedef {import('unist').Node} Node
+ */
 
 var hast2mdast = require('hast-util-to-mdast')
 
 module.exports = attacher
 
-// Attacher.
-// If a destination is given, runs the destination with the new mdast tree
-// (bridge-mode).
-// Without destination, returns the mdast tree: further plugins run on that tree
-// (mutate-mode).
 /**
- * @typedef {import('unified').Settings} Settings
- * @typedef {import('unified').Transformer} Transformer
- */
-/**
- * @param {Settings} destination
+ * Attacher.
+ *
+ * If a destination is given, runs the destination with the new mdast
+ * tree (bridge-mode). Without destination, returns the mdast tree: further
+ * plugins run on that tree (mutate-mode).
+ *
+ * @param {FrozenProcessor} destination
  * @param {Settings} options
  * @returns {Transformer}
  */
 function attacher(destination, options) {
+  /** @type {Partial<FrozenProcessor> & {document?: boolean | null}} */
   var settings
 
   if (destination && !destination.process) {
@@ -35,22 +40,31 @@ function attacher(destination, options) {
   return destination ? bridge(destination, settings) : mutate(settings)
 }
 
-// Bridge-mode.
-// Runs the destination with the new mdast tree.
+/**
+ * Bridge-mode.
+ * Runs the destination with the new mdast tree.
+ * @param {FrozenProcessor} destination
+ * @param {Settings} options
+ * @returns {Transformer}
+ */
 function bridge(destination, options) {
   return transformer
-  function transformer(node, file, next) {
-    destination.run(hast2mdast(node, options), file, done)
-    function done(err) {
-      next(err)
-    }
+  /** @type {Transformer} */
+  function transformer(node, file) {
+    destination.run(hast2mdast(node, options), file)
   }
 }
 
-// Mutate-mode.
-// Further transformers run on the mdast tree.
+/**
+ * Mutate-mode.
+ * Further transformers run on the mdast tree.
+ *
+ * @param {Settings} options
+ * @returns {Transformer}
+ */
 function mutate(options) {
   return transformer
+  /** @param {Node} node */
   function transformer(node) {
     return hast2mdast(node, options)
   }
